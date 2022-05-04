e5e2ca64395025214663ce3442ea2a2813803eb3
Johndoe <56298669+togane@users.noreply.github.com>
2022-05-05 08:47:25 +0900

    configure db for test

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -18,6 +18,6 @@ module.exports = {
     },
   },
   moduleNameMapper: {
-    '^@/(.+)': '<rootDir>/src/$1',
+    "^\\$backend/(.+)": "<rootDir>/src/$1",
   },
 };
--- backend/src/config/ormconfig.ts
+++ backend/src/config/ormconfig.ts
@@ -1,17 +1,24 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import TypeOrmNamingStrategy from './TypeOrmNamingStrategy';
 
+const database = process.env.DB_DATABASE ?? 'mnemonic_trainer';
+const databaseForTest = `${database}_test`;
+
 export const ormconfig: TypeOrmModuleOptions = {
   type: 'mysql',
   host: process.env.DB_HOST ?? 'host.docker.internal',
   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 33306,
   username: process.env.DB_USERNAME ?? 'root',
   password: process.env.DB_PASSWORD ?? 'password',
-  database: process.env.DB_DATABASE ?? 'mnemonic_trainer',
-  synchronize: false,
+  database: process.env.NODE_ENV === 'test' ? databaseForTest : database,
+  synchronize: process.env.NODE_ENV === 'test' ? true : false,
+  dropSchema: process.env.NODE_ENV === 'test' ? true : false,
   migrationsRun: false,
-  entities: ['dist/**/*.entity.js'],
+  entities:
+    process.env.NODE_ENV === 'test'
+      ? ['src/**/*.entity.ts']
+      : ['dist/**/*.entity.js'],
   migrations: ['dist/migrations/*.js'],
   namingStrategy: new TypeOrmNamingStrategy(),
-  logging: true,
+  logging: process.env.NODE_ENV === 'production' ? false : true,
 };
