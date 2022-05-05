330d27bdde60387e2a11d7cdaa5ddbeed11391e1
Johndoe <56298669+togane@users.noreply.github.com>
2022-05-05 17:16:32 +0900

    run a format script

--- backend/src/migrations/1651208282359-create-words.ts
+++ backend/src/migrations/1651208282359-create-words.ts
@@ -1,15 +1,18 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class createWords1651208282359 implements MigrationInterface {
-    name = 'createWords1651208282359'
+  name = 'createWords1651208282359';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE \`words\` (\`id\` int NOT NULL AUTO_INCREMENT, \`name\` varchar(255) NOT NULL, UNIQUE INDEX \`IDX_975efd50047f02b2266a8d8e9c\` (\`name\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE \`words\` (\`id\` int NOT NULL AUTO_INCREMENT, \`name\` varchar(255) NOT NULL, UNIQUE INDEX \`IDX_975efd50047f02b2266a8d8e9c\` (\`name\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP INDEX \`IDX_975efd50047f02b2266a8d8e9c\` ON \`words\``);
-        await queryRunner.query(`DROP TABLE \`words\``);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `DROP INDEX \`IDX_975efd50047f02b2266a8d8e9c\` ON \`words\``,
+    );
+    await queryRunner.query(`DROP TABLE \`words\``);
+  }
 }
