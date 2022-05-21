a3d8fdea04188a3ce2fd0690d6d8cc588460abe2
Johndoe <56298669+togane@users.noreply.github.com>
2022-05-21 13:22:02 +0900

    use validation pipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -6,8 +7,12 @@ async function bootstrap() {
   // TODO: CORS は最小限の範囲に絞る
   const app = await NestFactory.create(AppModule, { cors: true });
 
-  const config = new DocumentBuilder().setVersion('1.0').build();
-  const document = SwaggerModule.createDocument(app, config);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
+  const document = SwaggerModule.createDocument(
+    app,
+    new DocumentBuilder().setVersion('1.0').build(),
+  );
   SwaggerModule.setup('apidoc', app, document);
 
   await app.listen(3000);
